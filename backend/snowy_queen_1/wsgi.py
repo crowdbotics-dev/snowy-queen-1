5d31aee5ec3332b65719528f59de6466e3906b71
Crowdbotics <engineering@crowdbotics.com>
2024-04-09 17:36:22 +0000

    Initial commit

---
+++ backend/snowy_queen_1/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for snowy_queen_1 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "snowy_queen_1.settings"
+)
+
+application = get_wsgi_application()
